075d0783726a14c7659f1356ac18c6f32dd45aa8
cjafet <cjafet07@gmail.com>
2025-03-06 11:26:56 -0300

    Add login form

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
-import React, {useContext} from "react";
+import React, {useContext, useRef} from "react";
+// import { useNavigate } from "react-router-dom";
 import { ThemeContext } from "./context/ThemeContext";
 
 /**
@@ -9,26 +10,72 @@ import { ThemeContext } from "./context/ThemeContext";
  */
 const Home = () => {
     const { themeColor, actions } = useContext(ThemeContext);
+    const username = useRef(null);
+    const password = useRef(null);
 
-    const handleLogin = async (credential) => {  
+    const handleLogin = async (event) => {  
+        event.preventDefault();
+
         const credentials = {
-          userName: "comms",
-          password: "comms",
+            userName: username.current.value,
+            password: password.current.value,
         };
+
         const user = await actions.signIn(credentials);
           if (user) {
             console.log("Logged user", user.data);
             // navigate("/me");
           }
-      }
-    
-      handleLogin({});
+    }
+
+    const handleCancel = (event) => {
+        event.preventDefault();
+        // navigate("/");
+    };
+
     
     return (
 
-    <div style={{display: "inline-flex", alignItems: "center", height: "400px"}}>
-        <div style={{backgroundColor: themeColor, borderRadius: "8px", display: "flex", maxWidth: "581px"}}>
-            
+    <div style={{display: "inline-flex", alignItems: "center", width: "50%", height: "400px"}}>
+        <div style={{borderRadius: "8px", display: "block", width: "50%"}}>
+            <form onSubmit={handleLogin}>
+          <div className="form-group">
+            <input
+              type="username"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              ref={username}
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              ref={password}
+              placeholder="Password"
+            />
+          </div>
+          <div className="pad-bottom mt-3 btn-login">
+            <button
+              className="button"
+              type="submit"
+              style={{ backgroundColor: themeColor }}
+            >
+              Sign in
+            </button>
+            &nbsp;
+            <button
+              className="button button-secondary btn-login"
+              style={{ backgroundColor: themeColor }}
+              onClick={handleCancel}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
         </div>
     </div>
  )};
